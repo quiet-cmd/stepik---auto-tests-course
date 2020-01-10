f0ef5fefe3ddae197601ba97a45a8f930386258a
quiet-cmd <89250609951@yandex.ru>
2020-01-10 22:04:25 +0300

    open browser, input ,click button

--- week 1/lesson6_step4.py
+++ week 1/lesson6_step4.py
@@ -8,7 +8,7 @@ link = "http://suninjuly.github.io/find_link_text"
 try:
     browser = webdriver.Firefox()
     browser.get(link)
-    text = str(math.ceil(math.pow(math.pi, math.e) * 10000))  # зашифрованная ссылка
+    text = str(math.ceil(math.pow(math.pi, math.e) * 10000))
     link = browser.find_element_by_link_text(text)
     link.click()
 
