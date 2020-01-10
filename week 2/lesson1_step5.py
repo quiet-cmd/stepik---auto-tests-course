ab89fe69d3e963518814c11e39a89ab56e6c09a9
quiet-cmd <89250609951@yandex.ru>
2020-01-10 22:20:25 +0300

    Checkbox

--- week 2/lesson1_step5.py
+++ week 2/lesson1_step5.py
@@ -12,20 +12,16 @@ try:
     browser = webdriver.Firefox()
     browser.get(link)
 
-    x = browser.find_element_by_id("input_value")  # нашли тег с нашим числом
-    x = x.text  # взяли число х
-    x = calc(x)  # вставили х в формулу
-    input1 = browser.find_element_by_tag_name("input")  # нашли поле ввода
-    input1 = input1.send_keys(x)  # вввели в поле ввода наше число
+    x = browser.find_element_by_id("input_value")
+    x = x.text
+    x = calc(x)
+    input1 = browser.find_element_by_tag_name("input")
+    input1 = input1.send_keys(x)
     browser.find_element_by_id("robotCheckbox").click()
 
     browser.find_element_by_css_selector('#robotsRule').click()
     browser.find_element_by_css_selector("button.btn").click()
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
-
-    # пустая строка ;3
