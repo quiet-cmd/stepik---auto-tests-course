e53ccab505b7569c9386e8f44a3a87420fa6170e
quiet-cmd <89250609951@yandex.ru>
2020-01-10 22:29:05 +0300

    explicit expectation

--- week 2/lesson3_step8.py
+++ week 2/lesson3_step8.py
@@ -9,7 +9,6 @@ try:
     link = "http://suninjuly.github.io/explicit_wait2.html"
     browser = webdriver.Firefox()
     browser.get(link)
-    # говорим WebDriver искать каждый элемент в течение 5 секунд browser.implicitly_wait(5)
     WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
     browser.find_element_by_id("book").click()
     x = browser.find_element_by_id("input_value").text
