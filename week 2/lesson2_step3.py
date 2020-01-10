ee14cb6ac7f244aff576b0b4515a12cf43cb566a
quiet-cmd <89250609951@yandex.ru>
2020-01-10 22:22:59 +0300

    choose in dropdown list

--- week 2/lesson2_step3.py
+++ week 2/lesson2_step3.py
@@ -8,16 +8,12 @@ try:
     browser.get(link)
 
     x = int(browser.find_element_by_id("num1").text) + int(
-        browser.find_element_by_id("num2").text)  # взяли и сложили 2 числа
+        browser.find_element_by_id("num2").text)
 
-    select = Select(browser.find_element_by_id("dropdown"))  # нашли список значений
-    select.select_by_value(str(x))  # выбрали в нем значение x
+    select = Select(browser.find_element_by_id("dropdown"))
+    select.select_by_value(str(x))
     button = browser.find_element_by_class_name("btn").click()
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
-
-    # пустая строка ;3
